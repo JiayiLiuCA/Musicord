f07f699663c6b4dbca663e48b1e4ca8ac3c68714
JiayiLiuCA <liujiayi402@gmail.com>
2020-08-05 23:13:28 -0400

    Name, Room

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     return (
-        <div>
-            Join
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading"> Join </h1>
+                <div>
+                    <input
+                        placeholder="Name"
+                        className="joinInput"
+                        type="text"
+                        onChange={e => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                        placeholder="Room"
+                        className="joinInput"
+                        type="text"
+                        onChange={e => setRoom(e.target.value)}
+                    />
+                </div>
+                <Link
+                    //Name or room is empty, prevent click result
+                    onClick={e => (!name || !room) ? e.preventDefault() : null} 
+                    to={`/chat?name=${name}&room=${room}`}
+                >
+                    <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+            </div>
         </div>
     )
 }
